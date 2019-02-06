ef2d4027c2a6795d3b65b949150a5c1e92e43a12
Bohdan Dmytriv <dmytriv@quintagroup.org>
2019-02-06 09:49:04 +0200

    Use `search_list_with_dicts` from the op.api package

--- openprocurement/caravan/utils.py
+++ openprocurement/caravan/utils.py
@@ -78,23 +78,6 @@ def load_fixture(filepath, db):
         db.save(doc)
 
 
-def search_list_with_dicts(container, key, value):
-    """Search for dict in list with dicts
-
-    Useful for searching for milestone in the list of them.
-
-    :param container: an iterable to search in
-    :param key: key of dict to check
-    :param value: value of key to search
-
-    :returns: first acceptable dict
-    """
-    for item in container:
-        found_value = item.get(key, False)
-        if found_value and found_value == value:
-            return item
-
-
 def providedBy_or_error(interface, obj):
     if not interface.providedBy(obj):
         raise TypeError(
